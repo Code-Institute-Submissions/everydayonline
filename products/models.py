a5d9473f470ec6b4a010e46db73176b237b8bd8c
joenapper <joe.napper@hotmail.co.uk>
2020-08-21 19:28:53 +0000

    Added products app, models and fixtures

--- products/models.py
+++ products/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Category(models.Model):
     name = models.CharField(max_length=254)
@@ -14,7 +12,7 @@ class Category(models.Model):
         return self.friendly_name
 
 
-class Products(models.Model):
+class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
