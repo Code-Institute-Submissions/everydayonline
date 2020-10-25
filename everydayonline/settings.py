1867e1f888fc989fc1380ce415fef75ff5508e3a
joenapper <joe.napper@hotmail.co.uk>
2020-10-25 13:23:04 +0000

    Removed secret key and set debug

--- everydayonline/settings.py
+++ everydayonline/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jj8v(nqrt5j_z5a3jf^o(h7jwmxsc33s4q1o$zr1idnvknwrf&'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['everydayonline.herokuapp.com', 'localhost']
 
